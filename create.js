73a2135fa44d89dedd2337f18fa3d5ec6fe687f3
Carlos Martinez <martinezcj2@gmail.com>
2018-07-29 21:49:03 -0400

    Create create.js file

--- create.js
+++ create.js
@@ -1,29 +1,29 @@
 import uuid from "uuid";
 import AWS from "aws-sdk";
-AWS.config.update({ region: "us-east-2" }); //changed to 2
+AWS.config.update({ region: "us-east-1" });
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export function main(event, context, callback) {
- // Request body is passed in as a JSON encoded string in 'event.body'
- const data = JSON.parse(event.body);
- const params = {
-  TableName: "notes",
-  // 'Item' contains the attributes of the item to be created
-  // - 'userId': user identities are federated through the
-  // Cognito Identity Pool, we will use the identity id
-  // as the user id of the authenticated user
-  // - 'noteId': a unique uuid
-  // - 'content': parsed from request body
-  // - 'attachment': parsed from request body
-  // - 'createdAt': current Unix timestamp
-  Item: {
-    userId: event.requestContext.identity.cognitoIdentityId,
-    noteId: uuid.v1(),
-    content: data.content,
-    attachment: data.attachment,
-    createdAt: Date.now()
-  }
- };
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "notes",
+    // 'Item' contains the attributes of the item to be created
+    // - 'userId': user identities are federated through the
+    // Cognito Identity Pool, we will use the identity id
+    // as the user id of the authenticated user
+    // - 'noteId': a unique uuid
+    // - 'content': parsed from request body
+    // - 'attachment': parsed from request body
+    // - 'createdAt': current Unix timestamp
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now()
+    }
+};
   dynamoDb.put(params, (error, data) => {
     // Set response headers to enable CORS (Cross-Origin Resource Sharing)
     const headers = {
@@ -48,4 +48,4 @@ export function main(event, context, callback) {
   };
   callback(null, response);
   });
- }
+}
